b15427b05bc0beef9471356a84e87872bd24cd2d
AsyaPolyak <asyapolyachka@gmail.com>
2022-12-05 10:59:01 +0200

    added new project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div id="#app">
+  <MyComponents/>
+  <!--<my-components />-->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import MyComponents from './components/MyComponents.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    MyComponents
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MyComponents.vue
@@ -0,0 +1,40 @@
+<!--<template>
+    <div class="root">
+        {{text}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'MyComponents',
+    data() {
+        return {
+            text: 'hello'
+        }
+    }
+}
+
+</script>
+-->
+<template>
+    <div class="root">
+        <label >
+            Введіть email:
+            <input type="email" v-bind:value="asd">
+        </label>
+        <label >
+            Введіть пароль:
+            <input type="password" value="asd">
+        </label>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MyComponents',
+    data() {
+        return {
+            email:'nastia195@gmail.com',
+            password: '123455'
+        }
+    }
+}
+</script>
