2b24c85ff65087ebcad4693a9f59d9d5b5b96bdd
AsyaPolyak <asyapolyachka@gmail.com>
2022-12-07 15:27:59 +0200

    added push(newPost)

--- src/components/MyPost.vue
+++ src/components/MyPost.vue
@@ -2,11 +2,27 @@
 <!--import {createApp} from 'vue';-->
 <template>
     <div class="app">
-            <form >
+            <form @submit.prevent>
                 <h1>Створити пост</h1>
-                <input class="input" type="text" placeholder="post">
-                <input class="input" type="text" placeholder="post">
-                <button class="btn">Створити</button>
+                <input
+                  v-bind:value="title"
+                  @input="title = $event.target.value" 
+                  class="input"
+                  type="text"
+                  placeholder="post"
+                >
+                <input
+                  v-bind:value="body"
+                  @input="body = $event.target.value" 
+                  class="input"
+                  type="text"
+                  placeholder="post"
+                >
+                <button
+                  class="btn"
+                  @click="createPost">
+                  Створити
+                </button>
             </form>
         
         <div class="post" v-for="post in posts">
@@ -24,8 +40,24 @@ export default {
                 {id:1, title:'Java Script',body:'Опис поста'},
                 {id:2, title:'Java',body:'Опис поста'},
                 {id:3, title:'Python',body:'Опис поста'}
-        ]
+            ],
+            title: '',
+            body:  ''
         }
+    },
+    methods: {
+        createPost() {
+            const newPost ={
+                id: Date.now(),
+                title:this.title,
+                body:this.body
+            }
+            this.posts.push(newPost);
+            this.title = '',
+            this.body = ''
+        },
+
+
     }
 }
 </script>
